24d33fdd0156a05378bfb06f121d050a635e8094
JulSar <julien.sarraz01@gmail.com>
2017-05-17 17:44:16 +0200

    zohczihf

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-var AWS = require('aws-sdk');
 var express = require('express'), http = require('http');
 var app = express();
 
+var AWS = require("aws-sdk");
 
 AWS.config.update({
-	region: process.env.DYNAMO_REGION,
-	endpoint: process.env.DYNAMO_ENDPOINT
+    region: process.env.DYNAMO_REGION
 });
 
 app.get('/', function (req, res) {
-	res.send('Wow j\'ai modifié un truc...');
+    res.send('Wow - CI');
 });
 
-app.get('/students', function (req, res) {
+app.get('/users', function (req, res) {
 	
-
 });
 
-app.get('/user/:userId', function(req, res) {
+
+app.get('/user/:userId/:promo', function (req, res) {
 	var docClient = new AWS.DynamoDB.DocumentClient();
+	var user_id = req.params["userId"];
+	var promo = req.params["promo"]
 	var params = {
-		TableName: "users",
-		Key: {
-			"userId"; req.params["userId"];
-		}
-	}
-	docClient.get(params, function(err,data) {
-		if(err) {
-			res.send("Error while reading: " + JSON.stringify(err, nulln2));
+    	TableName: "users",
+    	Key:{
+        	"user_id": user_id,
+        	"promo": promo
+    	}
+	};
+
+	docClient.get(params, function(err, data) {
+		if (err) {
+		    res.send("Unable to read item. Error JSON:" + JSON.stringify(err, null, 2));
 		} else {
-			res.send("Get item: " + JSON.stringify(data, null, 2));
+	    	res.send("GetItem succeeded:" + JSON.stringify(data, null, 2));
 		}
 	});
-})
+});
 
 app.listen(process.env.PORT, function () {
-	console.log("server successfully started!");
+    console.log("Eventually server successfully started!");
 });
